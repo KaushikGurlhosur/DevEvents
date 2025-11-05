a54b38d78b85fa0d41a5806a46d49dc58caf3af3
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-11-05 16:00:23 +0530

    LightRays component from react bits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
+import LightRays from "@/components/LightRays";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibsted-grotesk",
@@ -26,7 +27,21 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} antialiased min-h-screen`}>
-        {children}
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={1.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0.1}
+            distortion={0.05}
+            className="custom-rays"
+          />
+        </div>
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 const Page = () => {
-  return <h1>Welcome to nextjs 16</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br />
+        Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, Meetups, and Conferences, All is One Place
+      </p>
+    </section>
+  );
 };
 
 export default Page;
