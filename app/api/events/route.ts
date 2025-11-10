87988848269ad6677d638e1fb30951eee0658ddb
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-11-10 13:52:19 +0530

    Implemented events creation api route

---
+++ app/api/events/route.ts
@@ -0,0 +1,41 @@
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import Event from "@/database/event.model";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries()); // Getting all entries like key value pairs from thr formData
+    } catch (e) {
+      return NextResponse.json(
+        {
+          message: "Invalid JSON data format",
+          error: e instanceof Error ? e.message : "Unknown",
+        },
+        { status: 400 }
+      );
+    }
+
+    const createdEvent = await Event.create(event);
+
+    return NextResponse.json(
+      { message: "Event Created Successfully", event: createdEvent },
+      { status: 201 }
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      {
+        message: "Event Creation Failed",
+        error: e instanceof Error ? e.message : "Unknown",
+      },
+      { status: 500 }
+    );
+  }
+}
