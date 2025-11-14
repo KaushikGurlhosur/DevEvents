562886174661a6d684516d8f0f59a67d72fdc531
Kaushik Gurlhosur <kaushikrg45@gmail.com>
2025-11-14 14:18:18 +0530

    Fetching events and nextjs config has been updated

---
+++ app/spline/page.jsx
@@ -0,0 +1,16 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Home() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/O0YT2CxJXvV-VsNd/scene.splinecode"
+        style={{
+          position: "absolute",
+          inset: 0,
+          background: "black",
+        }}
+      />
+    </main>
+  );
+}
